74720d97e9f903e3dce73ad80903432ba8fc14bb
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2022-03-24 19:58:02 -0300

    Estilos de calculadora casi terminados

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Boton from './components/Boton';
+import Pantalla from './components/Pantalla';
+import BotonClear from './components/BotonClear';
 
 function App() {
   return (
@@ -8,23 +10,35 @@ function App() {
         <h1 className='title' >Calculator using React Js</h1>
       </div>
       <div className='contenedor-calculadora' >
+        <Pantalla />
         <div className='fila'>
           <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
           <Boton>+</Boton>
         </div>
         <div className='fila'>
-
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
         </div>
         <div className='fila'>
-
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
         </div>
-
-
+        <div className='fila'>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear>Clear</BotonClear>
+        </div>
       </div>
-      <div className='fila'>
-
-      </div>
-
     </div>
   );
 }
