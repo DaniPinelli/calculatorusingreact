1355b7ddc9cb4f3853494b196a205dccdb8fb162
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2022-03-24 17:18:16 -0300

    Componente Boton creado y estilos asignados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-
 import './App.css';
+import Boton from './components/Boton';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Calculator</h1>
+    <div className='App'>
+      <div>
+        <h1 className='title' >Calculator using React Js</h1>
+      </div>
+      <div className='contenedor-calculadora' >
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='fila'>
+
+        </div>
+        <div className='fila'>
+
+        </div>
+
+
+      </div>
+      <div className='fila'>
+
+      </div>
+
     </div>
   );
 }
